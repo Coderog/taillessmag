f08376ff04db88df798f78a8303c899c8f305846
Leran Wang <2428592483@qq.com>
2018-04-06 12:51:14 +0800

    Update init.py
    To match the changes done in settings.py.

--- init.py
+++ init.py
@@ -5,7 +5,7 @@ conn = sqlite3.connect(DB_NAME)
 curs = conn.cursor()
 
 curs.execute('CREATE TABLE pages (id INT UNIQUE, url TEXT UNIQUE, done INT, title TEXT, content TEXT, linksto TEXT)')
-# curs.execute('INSERT INTO pages VALUES (0, ?, 0, \'\')', (START_URL,))
+curs.execute('INSERT INTO pages VALUES (0, ?, 0, \'\', \'\', \'\')', (input('Initial URL? '),))
 
 conn.commit()
 conn.close()
